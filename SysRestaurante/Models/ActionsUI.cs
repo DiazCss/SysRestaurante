f76f71066eee46e8c1b6c39e6506c790202d7503
Mena0904 <Walthermena0799@gmail.com>
2024-11-04 23:39:45 -0600

    actions ui para controles de formularios

---
+++ SysRestaurante/Models/ActionsUI.cs
@@ -0,0 +1,87 @@
+﻿namespace SysRestaurante.Models
+{
+    public class ActionsUI
+    {
+        public int Accion { get; set; }
+        public bool EsValidAction()
+        {
+            if (Accion == (int)ActionsUI_Enums.NUEVO ||
+                Accion == (int)ActionsUI_Enums.MODIFICAR ||
+                Accion == (int)ActionsUI_Enums.ELIMINAR ||
+                Accion == (int)ActionsUI_Enums.VER)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        public string ObtenerAccion()
+        {
+            if (Accion == (int)ActionsUI_Enums.NUEVO)
+                return "Create";
+            else if (Accion == (int)ActionsUI_Enums.MODIFICAR)
+                return "Edit";
+            else if (Accion == (int)ActionsUI_Enums.ELIMINAR)
+                return "Delete";
+            else if (Accion == (int)ActionsUI_Enums.VER)
+                return "Detail";
+            return "";
+
+        }
+        public string ObtenerTitulo(string pTexto)
+        {
+            if (Accion == (int)ActionsUI_Enums.NUEVO)
+                return "Crear " + pTexto;
+            else if (Accion == (int)ActionsUI_Enums.MODIFICAR)
+                return "Modificar " + pTexto;
+            else if (Accion == (int)ActionsUI_Enums.ELIMINAR)
+                return "Eliminar " + pTexto;
+            else if (Accion == (int)ActionsUI_Enums.VER)
+                return "Ver " + pTexto;
+            return "";
+
+        }
+        public string ObtenerNombreBoton()
+        {
+            if (Accion == (int)ActionsUI_Enums.NUEVO)
+                return "Crear";
+            else if (Accion == (int)ActionsUI_Enums.MODIFICAR)
+                return "Modificar";
+            else if (Accion == (int)ActionsUI_Enums.ELIMINAR)
+                return "Eliminar";
+            else if (Accion == (int)ActionsUI_Enums.VER)
+                return "Ir a modificar";
+            return "";
+
+        }
+        public string ObtenerAccionJs()
+        {
+            if (Accion == (int)ActionsUI_Enums.NUEVO)
+                return "crear";
+            else if (Accion == (int)ActionsUI_Enums.MODIFICAR)
+                return "modificar";
+            else if (Accion == (int)ActionsUI_Enums.ELIMINAR)
+                return "eliminar";
+            else if (Accion == (int)ActionsUI_Enums.VER)
+                return "ver";
+            return "";
+
+        }
+        public bool SiTraerDatos()
+        {
+            if (Accion == (int)ActionsUI_Enums.NUEVO)
+                return false;
+            else
+                return true;
+        }
+    }
+    public enum ActionsUI_Enums
+    {
+        NUEVO = 1,
+        MODIFICAR = 2,
+        ELIMINAR = 3,
+        VER = 4,
+        IMPRIMIR = 5
+    }
+}
+
--- SysRestaurante/wwwroot/js/site.js
+++ SysRestaurante/wwwroot/js/site.js
@@ -1,4 +1,32 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿// Write your JavaScript code.
+var ActionsUI = {
+    init: function (pAction) {
+        $("*[data-disabledui_" + pAction + "]").attr("disabled", "disabled");
+        $("*[data-readonlyui_" + pAction + "]").attr("readonly", "readonly");
+    },
+    create: function (pAction, pFunc) {
+        if (pAction == "crear") {
+            pFunc();
+        }
+    },
+    edit: function (pAction, pFunc) {
+        if (pAction == "modificar") {
+            pFunc();
+        }
+    },
+    delete: function (pAction, pFunc) {
+        if (pAction == "eliminar") {
+            pFunc();
+        }
+    },
+    detail: function (pAction, pFunc) {
+        if (pAction == "ver") {
+            pFunc();
+        }
+    },
+    allEx: function (pAction, pActionEx, pFunc) {
+        if (pAction != pActionEx) {
+            pFunc();
+        }
+    }
+}
