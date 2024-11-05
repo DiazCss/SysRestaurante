c67edbb201362468f8c0609e46d775e0ad79472f
Mena0904 <Walthermena0799@gmail.com>
2024-11-04 21:49:19 -0600

    Creacion de Dependency Containeren capa DAL

---
+++ SysRestaurante.DAL/DependencyContainer.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SysRestaurante.BL.DTOs;
+using SysRestaurante.BL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysRestaurante.DAL
+{
+   
+    public static class DependecyContainer
+    {
+        public static IServiceCollection AddDALDependecies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<SysRestauranteDbContext>(options =>
+            options.UseMySql(configuration.GetConnectionString("Conn"), ServerVersion.AutoDetect(configuration.GetConnectionString("Conn"))));
+            return services;
+        }
+    }
+}
