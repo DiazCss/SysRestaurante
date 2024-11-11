2359a74dfb6ecc4ebfe84c91750562bd9e50eb8e
Antomarr14 <diegozetino8@gmail.com>
2024-11-10 21:44:04 -0600

    Agregacion de la clase ActionsDTO

---
+++ SysRestaurante.BL.DTOs/ActionsDTO.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysRestaurante.BL.DTOs
+{
+    public class ActionsDTO
+    {
+        /// <summary>
+        /// Usar el enum de Actions_enum par saber la accion  a utilizar
+        /// </summary>
+        public byte Accion_aux { get; set; }
+
+    }
+    public enum Actions_enum
+    {
+        NUEVO = 1,
+        MODIFICAR = 2,
+        ELIMINAR = 3
+    }
+}
